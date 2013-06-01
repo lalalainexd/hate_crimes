5577dacffc213d0078f95bdf200bf12bdeeaca78
lalalainexd <lalalainexd@gmail.com>
2013-06-01 10:30:25 -0600

    removed extra routes and delted vies that aren't used

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 HateMap::Application.routes.draw do
-  root to: 'victim_counts#index'
+  root to: 'application#index'
 
-  resources :biases, only: [:show, :index]
-  resources :offenses, only: [:show, :index]
-  # /api/v3/turtle/powers
-  # /api/turtle/powers
+  #resources :biases, only: [:show, :index]
+  #resources :offenses, only: [:show, :index]
 
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: Router::ApiConstraints.new(version: 1, default: true) do
-      resources :victim_counts
+      resources :victim_counts, only: [:index]
     end
   end
 
